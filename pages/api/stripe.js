02a6d437ad8c13e3ed6b2319505436cf7e711970
Furkan Altun <faltun@architech.ca>
2023-06-23 03:10:59 -0400

    added promo option

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
       const params = {
         submit_type: 'pay',
         mode: 'payment',
+        allow_promotion_codes: true,
         payment_method_types: ['card', 'blik'],
         shipping_address_collection: {
           allowed_countries: ["US", "CA", "KE", "PL"],
